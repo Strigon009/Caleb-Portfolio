b50b3bf326ba1bbec73b5c6f8f6e305de32b44d5
Caleb Rayman Som <calebraymansom@gmail.com>
2024-12-04 20:54:44 +0800

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+﻿// Get all the anchor links
+const links = document.querySelectorAll('a.header-textLink');
+
+// Get the current page URL
+const currentPage = window.location.href;
+
+// Loop through each link and compare its href with the current page
+links.forEach(link => {
+    const linkUrl = link.href;
+    if (currentPage === linkUrl) {
+        // Add the 'active' class to the matching link
+        link.classList.add('active');
+    }
+});
